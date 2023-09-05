366b627709c8e2bcb4d966383ede9384d34048b3
Ibrahim Khan <73376843+Mribrahimkhan360@users.noreply.github.com>
2023-09-06 00:38:21 +0600

    Update STL-1.cpp

--- STL-1.cpp
+++ STL-1.cpp
@@ -16,7 +16,24 @@ int main()
 {
     ios_base::sync_with_stdio(false);
     cin.tie(NULL);
-    vector<int> v1{1,2,3,4,5};
-    print(v1);
+    //    vector<int> data;
+//    print(data);
+
+//    vector<int> data(5, 2);
+//    print(data);
+
+//    vector<int> data({1, 2, 3, 4, 5}); /// int data[] = {1, 2, 3, 4, 5}
+//    print(data);
+
+//    vector<int> data = {1, 2, 3, 4, 5}; /// int data[] = {1, 2, 3, 4, 5}
+//    print(data);
+
+//    vector<int> data = {1, 2, 3, 4, 5}; /// int data[] = {1, 2, 3, 4, 5}
+//    vector<int> data2 = data;
+//    print(data2);
+
+    vector<int> data = {1,2,3,4,5};
+    vector<int> data2 = data;
+    print(data2);
 
 }
