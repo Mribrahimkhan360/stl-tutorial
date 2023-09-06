5e69231f90b70b64187a30bdbcb60c7c6747f2a9
Ibrahim Khan <73376843+Mribrahimkhan360@users.noreply.github.com>
2023-09-06 13:31:27 +0600

    Add files via upload

---
+++ Initialize Vector in C++.cpp
@@ -0,0 +1,13 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int elements =12;
+    vector<int> vec(elements,8);
+
+    for(int i = 0; i<vec.size();i++)
+    {
+        cout<<vec[i]<< " \n ";
+    }
+    return 0;
+}
