a1020e08447d8d85005c45d5c9e7ab70c6682628
Ibrahim Khan <73376843+Mribrahimkhan360@users.noreply.github.com>
2023-09-07 13:24:13 +0600

    Add files via upload

---
+++ Deque.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+#include<deque>
+using namespace std;
+int main()
+{
+    deque<int> dq;
+    dq.push_back(1);
+    dq.push_back(2);
+    dq.push_back(3);
+
+    for(int i=0;i<dq.size();i++)
+    {
+        cout<<dq[i]<<" ";
+    }
+    return 0;
+}
