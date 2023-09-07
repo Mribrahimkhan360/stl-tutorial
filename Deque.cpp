749246cd54cef5ef96c6e9f9f5903ab29bae0c33
Ibrahim Khan <73376843+Mribrahimkhan360@users.noreply.github.com>
2023-09-07 13:30:20 +0600

    Update Deque.cpp

--- Deque.cpp
+++ Deque.cpp
@@ -13,11 +13,25 @@ int main()
 //        cout<<dq[i]<<" ";
 //    }
 //=================================
+//
+//    deque<int> dq;
+//    dq.push_back(1);
+//    dq.push_back(2);
+//    dq.push_back(3);
+//
+//    for(int i=0; i<dq.size(); i++)
+//    {
+//        cout<<dq.at(i)<<" ";
+//    }
+
+    //=================================
 
     deque<int> dq;
     dq.push_back(1);
     dq.push_back(2);
     dq.push_back(3);
+    dq.push_front(4);
+    dq.push_back(5);
 
     for(int i=0; i<dq.size(); i++)
     {
