5adc5ec365f980a053d0185bb37475314ef47930
Ibrahim Khan <73376843+Mribrahimkhan360@users.noreply.github.com>
2023-09-07 13:44:17 +0600

    Update Deque.cpp

--- Deque.cpp
+++ Deque.cpp
@@ -26,16 +26,25 @@ int main()
 
     //=================================
 
-    deque<int> dq;
-    dq.push_back(1);
-    dq.push_back(2);
-    dq.push_back(3);
-    dq.push_front(4);
-    dq.push_back(5);
+//    deque<int> dq;
+//    dq.push_back(1);
+//    dq.push_back(2);
+//    dq.push_back(3);
+//    dq.push_front(4);
+//    dq.push_back(5);
+//    dq.clear();
+    deque<int> dq={1,2,3,5,6,7,8,9,10};
 
     for(int i=0; i<dq.size(); i++)
     {
         cout<<dq.at(i)<<" ";
     }
+
+
+
+    cout<<endl<<" "<<"front and back";
+
+    cout<<endl<<dq.front();
+    cout<<endl<<dq.back();
     return 0;
 }
